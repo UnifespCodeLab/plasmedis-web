18ccef764a1be834fd11c8982e7ab35f21d79022
CaioAbe02 <66100834+CaioAbe02@users.noreply.github.com>
2024-08-28 20:34:52 -0300

    feat add new methods

--- src/domain/categorias/index.js
+++ src/domain/categorias/index.js
@@ -1,2 +1,4 @@
 export {default as getAll} from './getAll';
 export {default as deleteById} from './deleteById';
+export {default as create} from './create';
+export {default as updateById} from './updateById';
